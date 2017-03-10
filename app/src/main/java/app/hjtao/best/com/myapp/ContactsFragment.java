c06330550206d377d031bb7355f98a2615b751cf
Taogeg <981372386@qq.com>
2017-03-10 20:52:16 +0800

    fragment

---
+++ app/src/main/java/app/hjtao/best/com/myapp/ContactsFragment.java
@@ -0,0 +1,23 @@
+package app.hjtao.best.com.myapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2017/3/10.
+ */
+public class ContactsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab2, container, false);
+        return view;
+    }
+
+
+    
+}
---
+++ app/src/main/java/app/hjtao/best/com/myapp/WeixinFragment.java
@@ -0,0 +1,22 @@
+package app.hjtao.best.com.myapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2017/3/10.
+ */
+
+public class WeixinFragment extends Fragment {
+    @Nullable
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab1, container, false);
+        return view;
+    }
+}
+
